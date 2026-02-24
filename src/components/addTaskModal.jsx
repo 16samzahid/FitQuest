9419d80cd3a001cba68a3d26086465eb7cb36881
Sameeha Zahid <sam.zahid103@icloud.com>
2026-02-24 16:24:13 +0000

    adding to add task modal

--- src/components/addTaskModal.jsx
+++ src/components/addTaskModal.jsx
@@ -1,10 +1,19 @@
 import Checkbox from "expo-checkbox";
 import { useState } from "react";
 import { Modal, Pressable, Text, TextInput, View } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
 
 export default function AddTaskModal({ visible, onClose, onCreate }) {
   const [description, setDescription] = useState("");
   const [approvalNeeded, setApprovalNeeded] = useState(true);
+  const [category, setCategory] = useState(null);
+  const categories = [
+    { label: "Exercise", value: "Exercise" },
+    { label: "Learning", value: "Learning" },
+    { label: "Hygiene", value: "Hygiene" },
+    { label: "Food", value: "Food" },
+    { label: "Play", value: "Play" },
+  ];
 
   const handleCreate = () => {
     if (!description.trim()) return;
@@ -35,6 +44,26 @@ export default function AddTaskModal({ visible, onClose, onCreate }) {
             />
             <Text className="ml-3 text-gray-700">Requires Parent Approval</Text>
           </View>
+          {/* Category Dropdown */}
+          <Dropdown
+            data={categories}
+            labelField="label"
+            valueField="value"
+            placeholder="Select Category"
+            value={category}
+            onChange={(item) => {
+              setCategory(item.value);
+            }}
+            className="mb-6"
+            containerStyle={{
+              borderColor: "#E5E7EB",
+              borderWidth: 1,
+              borderRadius: 8,
+            }}
+            placeholderStyle={{ color: "#9CA3AF" }}
+            selectedTextStyle={{ color: "#111827" }}
+            inputSearchStyle={{ color: "#111827" }}
+          />
 
           <View className="flex-row justify-end space-x-3">
             <Pressable
