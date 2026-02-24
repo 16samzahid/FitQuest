d642daec35065ace522614cdc1a1b2ebe0856bc7
Sameeha Zahid <sam.zahid103@icloud.com>
2026-02-24 16:08:28 +0000

    user can specify task desc and approvalNeeded

--- src/screens/parent/ManageTasks.jsx
+++ src/screens/parent/ManageTasks.jsx
@@ -1,22 +1,29 @@
 import { useAppData } from "@/src/context/AppDataContext";
+import { useState } from "react";
 import { Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import AddTaskModal from "../../components/addTaskModal";
 import { createTask } from "../../services/taskService";
 
 const ManageTasks = () => {
   const { child } = useAppData();
-  const handleCreateTask = async () => {
-    alert("Creating a new task for the child...");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleCreateTask = async (description, approvalNeeded) => {
+    if (!child) {
+      alert("No child selected");
+      return;
+    }
     try {
       await createTask({
-        approvalNeeded: true,
+        approvalNeeded: approvalNeeded,
         approvedBy: null,
         category: "Exercise",
         childID: child.id,
         coins: 10,
         completedAt: null,
         createdAt: new Date(),
-        description: "Do 20 push-ups",
+        description: description,
         dueDate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // Due in 7 days
         recurrence: null,
         status: "notdone",
@@ -33,10 +40,18 @@ const ManageTasks = () => {
         <Text>ManageTasks</Text>
         <Pressable
           className="mt-4 bg-indigo-600 py-3 rounded-full items-center"
-          onPress={handleCreateTask}
+          onPress={() => setModalVisible(true)}
         >
           <Text className="text-white font-semibold">Create New Task</Text>
         </Pressable>
+
+        <AddTaskModal
+          visible={modalVisible}
+          onClose={() => setModalVisible(false)}
+          onCreate={({ description, approvalNeeded }) =>
+            handleCreateTask(description, approvalNeeded)
+          }
+        />
       </View>
     </SafeAreaView>
   );
