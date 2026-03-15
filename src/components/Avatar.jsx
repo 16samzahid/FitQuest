e991640bcf10ae8229207f2eeabed8451255eeeb
Sameeha Zahid <sam.zahid103@icloud.com>
2026-03-15 17:25:40 +0000

    avatar now loads any equipped accessories

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,8 +1,36 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { ActivityIndicator, Image, View } from "react-native";
+import { db } from "../../config/FirebaseConfig";
 import { useAppData } from "../context/AppDataContext";
 
 export default function Avatar({ width = 300, height = 300 }) {
-  const { pet, loading } = useAppData();
+  const { pet, loading, childAccessories } = useAppData();
+  const [accessories, setAccessories] = useState([]);
+  useEffect(() => {
+    const fetchAccessories = async () => {
+      try {
+        const snap = await getDocs(collection(db, "Accessories"));
+
+        const list = snap.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setAccessories(list);
+      } catch (error) {
+        console.error("Error fetching accessories:", error);
+      }
+    };
+
+    fetchAccessories();
+  }, []);
+  console.log(
+    "Rendering Avatar with pet:",
+    pet,
+    "and accessories:",
+    childAccessories,
+  );
 
   if (loading) {
     return <ActivityIndicator size="large" />;
@@ -31,6 +59,30 @@ export default function Avatar({ width = 300, height = 300 }) {
         }}
         resizeMode="contain"
       />
+
+      {/* Accessories */}
+      {childAccessories
+        ?.filter((a) => a.equipped)
+        .map((childAccessory) => {
+          const accessory = accessories.find(
+            (a) => a.id === childAccessory.accessoryID,
+          );
+
+          if (!accessory) return null;
+
+          return (
+            <Image
+              key={childAccessory.id}
+              source={{ uri: accessory.imageURL }}
+              style={{
+                position: "absolute",
+                width,
+                height,
+              }}
+              resizeMode="contain"
+            />
+          );
+        })}
     </View>
   );
 }
